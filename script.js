c65274a14ce04bbe53b19e96264c00b49bd33819
irina mihai <irinamihai150@gmail.com>
2022-10-17 21:57:26 +0100

    adding levels and fixing bugs

--- script.js
+++ script.js
@@ -1,19 +1,58 @@
 //You can edit ALL of the code here
 const rootElem = document.getElementById("root");
-const allEpisodes = getAllEpisodes();
+//level 350
+let allEpisodes = null;
 
 function setup() {
-  const allEpisodes = getAllEpisodes();
-  makePageForEpisodes(allEpisodes);
+  fetch("https://api.tvmaze.com/shows/581/episodes")
+    .then((response) => {
+      return response.json();
+    })
+    .then((data) => {
+      allEpisodes = data;
+      makePageForEpisodes(data);
+    });
 }
+
+//level 400
+let selectShow = document.getElementById("select-show");
+let allShows = getAllShows();
+allShows.forEach((show) => {
+  let option = document.createElement("option");
+  selectShow.appendChild(option);
+  option.text = show.name;
+  let sortedShows = allShows.sort((a, b) => a.name.localeCompare(b.name));
+});
+
+selectShow.addEventListener("change", (ev) => {
+  let selectedShow = allShows.filter((show) => {
+    if (ev.target.value === show.name) {
+      return show;
+      console.log(show.name);
+    }
+    console.log(ev.target.value);
+  });
+
+  let showId = selectedShow[0].id;
+  fetch(`https://api.tvmaze.com/shows/${showId}/episodes`)
+    .then((response) => {
+      return response.json();
+    })
+    .then((data) => {
+      console.log(data);
+      makePageForEpisodes(data);
+    });
+});
 
 function makePageForEpisodes(episodeList) {
   const rootElem = document.getElementById("root");
   rootElem.textContent = `Got ${episodeList.length} episode(s)`;
   rootElem.innerHTML = "";
 
+  const select = document.getElementById("select");
+  const selectbar = document.getElementById("selectbar");
+  select.parentElement.removeChild(select);
   episodeList.forEach((episode) => {
-    //container for h1,h2, img, p
     const divEpisode = document.createElement("div");
     //title
     const titleEpisode = document.createElement("h1");
@@ -37,19 +76,21 @@ function makePageForEpisodes(episodeList) {
     //adding content
     // titleEpisode.innerHTML = episodeName;
     seasonEpisode.innerHTML = `${episodeName}-S0${seasonNumber}E0${episodeNumber}`;
-    imageEpisode.src = episode.image.medium;
+    if (episode.image) {
+      imageEpisode.src = episode.image.medium;
+    }
+    // imageEpisode.src = episode.image.medium;
     summary.innerHTML = episode.summary;
-      divEpisode.classList.add("container");
-      rootElem.appendChild(divEpisode);
+    divEpisode.classList.add("container");
+    rootElem.appendChild(divEpisode);
   });
+
   //level 200
-  //add a search input field
-  let searchField= document.getElementById("search");
-  let searchValue="";
-  // let searchValue = document.getElementsByClassName("displaySearch");
-  
+
+  let searchField = document.getElementById("search");
+  let searchValue = "";
   //create a function to filter episodes
-  searchField.addEventListener("keydown", (ev)=>{
+  searchField.addEventListener("keydown", (ev) => {
     searchValue = ev.target.value.toLowerCase();
     //The target event property returns the element that triggered the event.
     let searchedEpisodes = allEpisodes.filter(
@@ -57,43 +98,42 @@ function makePageForEpisodes(episodeList) {
         episode.name.toLowerCase().includes(searchValue) ||
         episode.summary.toLowerCase().includes(searchValue)
     );
-    console.log("debugger but" + searchedEpisodes.length)
+    console.log("debugger but" + searchedEpisodes.length);
     // rootElem.innerHtml = "";
     //recall the function to recreate the page for searchValue
-    
+
     let displayNumSearched = document.getElementById("displaySearch");
-    console.log(displayNumSearched)
+    console.log(displayNumSearched);
     displayNumSearched.textContent = `Display${searchedEpisodes.length}/ ${allEpisodes.length} episodes`;
     // displayNumSearched.appendChild
     makePageForEpisodes(searchedEpisodes);
-  })
-  
+  });
+
   //level 300
-  //create select and option
-  let select = document.getElementById("select");
-  allEpisodes.forEach(episode=>{
+  let selectElm = document.createElement("select");
+  selectElm.id = "select";
+  selectbar.appendChild(selectElm);
+  episodeList.forEach((episode) => {
     let option = document.createElement("option");
-    select.appendChild(option)
-    //the text property sets the text of an option element
-    option.text = `SO${episode.season}E${episode.number}-${episode.name}`
+    selectElm.appendChild(option);
+    option.text = `SO${episode.season}E${episode.number}-${episode.name}`;
     option.value = episode.id;
-    // console.log(option.value)
-  })
-  select.addEventListener("change", (ev)=>{
-    console.log(allEpisodes);
-    
-    let selectedEpisodes = allEpisodes.filter(episode => episode.id.toString()===ev.target.value);
-    // console.log(ev.target.value);
-    console.log("selectedEpisodes"+selectedEpisodes);
+  });
+  selectElm.addEventListener("change", (ev) => {
+    let selectedEpisodes = episodeList.filter(
+      (episode) => episode.id.toString() === ev.target.value
+    );
+
+    console.log("selectedEpisodes" + selectedEpisodes);
     rootElem.innerHTML = "";
     makePageForEpisodes(selectedEpisodes);
-    let displayNumSearched = document.getElementById("displaySearch")
-    displayNumSearched.innerHTML = `Display${selectedEpisodes.length}/ ${allEpisodes.length} episodes`;
-  })
-  document.getElementById("reset").addEventListener("click", ()=> {
-     makePageForEpisodes(allEpisodes);
-     select.value = -1;
-  })
+    let displayNumSearched = document.getElementById("displaySearch");
+    displayNumSearched.innerHTML = `Display${selectedEpisodes.length}/ ${episodeList.length} episodes`;
+  });
+  document.getElementById("reset").addEventListener("click", () => {
+    makePageForEpisodes(allEpisodes);
+    select.value = -1;
+  });
 }
 
 //create footer
@@ -103,7 +143,5 @@ let copyRight = document.createElement("p");
 footer.appendChild(copyRight);
 copyRight.innerHTML =
   'The data originally comes from <a href="https://www.tvmaze.com/">TVMaze.com</a>';
-
-
 
 window.onload = setup;
